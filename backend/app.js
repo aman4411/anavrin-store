5864477216ce35396f6304db395c373e65653171
Aman Mehta <amanmehta4411@gmail.com>
2022-02-16 00:30:46 +0530

    basic route setup

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
+//import routes
+const productRoute = require("./routes/productRoute");
+//use routes
+app.use("/api",productRoute);
 
 module.exports = app;
