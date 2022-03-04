3933116d77358786f6103c6f0d1c2d6eaa5a75ec
Aman Mehta <amanmehta4411@gmail.com>
2022-03-05 00:53:44 +0530

    footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
-import {BrowserRouter as Router } from 'react-router-dom';
-import Header from './component/layout/Header/Header';
+import {BrowserRouter as Router } from 'react-router-dom'
+import Header from './component/layout/Header/Header'
 import ReactNavbar from 'overlay-navbar/dist/lib/ReactNavbar';
+import WebFont from 'webfontloader'
 import './App.css';
+import React, { useEffect } from 'react';
+import Footer from './component/layout/Footer/Footer';
+
 
 function App() {
+
+  React.useEffect(() => {
+    WebFont.load({
+      google:{
+        families:["Roboto","Droid Sans","Chilanka"]
+      }
+    });
+  });
   return (
     <Router>
       <Header/>
+      <Footer/>
     </Router>
   );
 }
