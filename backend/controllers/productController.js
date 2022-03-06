4daf19bd5fa1a75b16eafe0ec09506317a667e7c
Aman Mehta <amanmehta4411@gmail.com>
2022-03-07 00:07:00 +0530

    products reducer

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -15,7 +15,7 @@ exports.createProduct = catchAsyncError(async (req, resp, next) => {
 
 //get all products
 exports.getAllProducts = catchAsyncError(async (req, resp, next) => {
-    const resultPerPage = 5;
+    const resultPerPage = 8;
     const productCount = await Product.countDocuments();
     const apiFeature = new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage);
     const products = await apiFeature.query;
--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,17 +1,21 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { CgMouse } from 'react-icons/cg';
 import './Home.css';
 import Product from './Product.js';
 import MetaData from '../layout/MetaData';
-
-const product = {
-    name:"Blue Tshirt",
-    images:[{url:"https://i.ibb.co/DRST11n/1.webp"}],
-    price:"3000",
-    _id:"product1"
-};
+import { getProduct } from '../../actions/productAction';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Home = () => {
+  
+    const dispatch = useDispatch();
+    const {loading,error,products,productsCount} = useSelector(
+        (state) => state.products
+    );
+    useEffect(() => {
+        dispatch(getProduct());
+    }, [dispatch])
+    
   return (
     <Fragment>
         <MetaData title={"Anavrin Store : Get best quality products here"}/>
@@ -27,14 +31,9 @@ const Home = () => {
 
         <h2 className='homeHeading'>Featured Products</h2>
         <div className="container" id='container'>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
-            <Product product={product}/>
+            {products && products.map(product => (
+                <Product product={product}/>
+            ))}
         </div>
     </Fragment>
   )
